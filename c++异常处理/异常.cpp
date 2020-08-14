e18a693ed82a51abcaf0380daaf4282be4e19c4c
zhjwang <zhjwang@thoughtworks.com>
2020-08-14 16:49:01 +0800

    [zhjwang] 异常demo

---
+++ c++异常处理/异常.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try{
+        int *p = new int[99999999];
+        cout << "test" << endl;  //假设上面一条出现了异常，那么这行之下的代码就不会再被执行了。
+    }catch(...){  //...表示可以捕获任意的异常
+        cout << "产生了异常" << endl;
+    }
+    return 0;
+}
