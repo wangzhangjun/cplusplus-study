30a37af713d59e47d8ad6175ce0bce2e07a4d084
zhjwang <695331215@qq.com>
2021-01-28 15:27:08 +0800

    update

---
+++ c++-文件操作/对象不可复制或赋值.cpp
@@ -0,0 +1,14 @@
+//
+// Created by zhjwang on 2021/1/18.
+//
+
+#include <iostream>
+#include <fstream>
+
+int main(){
+    std::fstream fs, fs2;
+    fs = fs2;
+    std::cout << "hello" << std::endl;
+    return 0;
+}
+
