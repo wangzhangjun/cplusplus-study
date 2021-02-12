60630a7b956285ed999ddf16d2bbfee4f73992b6
zhjwang <695331215@qq.com>
2021-02-12 20:26:08 +0800

    string构造注释

--- c++-stl/string.cpp
+++ c++-stl/string.cpp
@@ -6,6 +6,12 @@
 #include <string>
 
 //string构造
+/*
+ * 1. sring() //构造一个口弄个的字符串
+ * 2. string(const string &str) //构造一个与str一样的string。如string s1(s2)。
+ * 3. string(const char *s);    //用字符串s初始化
+ * 4. string(int n,char c);    //用n个字符c初始化
+ * */
 void main01()
 {
     std::string s1 = "123";
