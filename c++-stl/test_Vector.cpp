6b1f4e85cb36fdae211e397a927997957746c8a1
zhjwang <695331215@qq.com>
2021-02-13 07:46:21 +0800

    vector

--- c++-stl/test_Vector.cpp
+++ c++-stl/test_Vector.cpp
@@ -5,12 +5,17 @@
 #include <string>
 #include <vector>
 
-void main08()
+void main09()
 {
-
+    std::vector<int> v1;
+    std::cout << v1.size() << std::endl; //0
+    v1.push_back(1);
+    v1.push_back(3);
+    v1.push_back(5);
+    std::cout << v1.size() << std::endl; //3
 }
 int main()
 {
-    main08();
+    main09();
     return 0;
 }
