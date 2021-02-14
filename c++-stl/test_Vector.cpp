04e6799ada2ce33ed558b2ff99910ef589c84adf
zhjwang <695331215@qq.com>
2021-02-14 21:54:00 +0800

    vector

--- c++-stl/test_Vector.cpp
+++ c++-stl/test_Vector.cpp
@@ -15,7 +15,7 @@ void main09()
     std::cout << v1.size() << std::endl; //3
 
     std::cout << "头部元素：" << v1.front() << std::endl; //获取头部元素
-    v1.front() = 11;//修改头部元素， 函数返回值当左值，应该返回一个引用
+    v1.front() = 11;//修改头部元素,函数返回值当左值,应该返回一个引用
 
     while(v1.size() > 0) {
         std::cout << v1.back() << std::endl; //获取尾部元素
@@ -26,14 +26,15 @@ void main09()
 //vector的初始化
 void main10()
 {
-    std::vector<int> v1;
+    std::vector<int> v1;  //空的vector
     v1.push_back(1);
     v1.push_back(3);
     v1.push_back(5);
     v1.push_back(7);
 
-    std::vector<int> v2 = v1; //对象初始化
+    std::vector<int> v2 = v1; //用对象初始化
 
+    //构造函数将[beg, end)区间中的元素拷贝给本身。注意该区间是左闭右开的区间。
     std::vector<int> v3(v1.begin(), v1.begin()+2);
 }
 
@@ -58,9 +59,34 @@ void main11()
     printV(v1);
 }
 
+//迭代器
+void main12()
+{
+    std::vector<int> v1(10); //提前把内存准备好
+    for(int i = 0; i < 10; i++)
+    {
+        v1[i] = i + 1;
+    }
+    //正向遍历
+    for(std::vector<int>::iterator it = v1.begin(); it != v1.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+    //逆向遍历
+    for(std::vector<int>::reverse_iterator rit = v1.rbegin(); rit != v1.rend(); ++rit){
+        std::cout << *rit << std::endl;
+    }
+}
+
+//删除
+void main13()
+{
+    
+}
+
 int main()
 {
     //main09();
-    main11();
+//    main11();
+    main12();
     return 0;
 }
