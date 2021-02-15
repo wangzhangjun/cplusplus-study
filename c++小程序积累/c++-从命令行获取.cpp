908d926cacb77fd74df39686ebb986f63288a2be
zhjwang <695331215@qq.com>
2021-02-15 23:32:21 +0800

    从命令行获取

---
+++ c++小程序积累/c++-从命令行获取.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+从命令行获取如下：
+
+2  8
+123456
+12
+
+*/
+void main01()
+{
+    int colomns;
+    int len;
+    string str = "";
+    std::vector<std::string> svec;
+
+    cout << "请输入colomns:" << endl;
+    cin >> colomns;
+    cout << "请输入len:" << endl;
+    cin >> len;
+    cout << "请输入str:" << endl;
+    for (int i = 0; i < colomns; i++)
+    {
+        cin >> str; //读取字符串，不含空格
+        svec.push_back(str);
+        str = "";
+    }
+    //遍历
+    for (int i = 0; i < svec.size(); i++)
+    {
+        std::cout << svec[i] << std::endl;
+        for (int j = 0; j < svec[i].length(); j++)
+        {
+            std::cout << svec[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    main01();
+    return 0;
+}
