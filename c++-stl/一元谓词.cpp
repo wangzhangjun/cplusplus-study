d94ea92215d349f3b02380cdcfbb70ddffaac20d
zhjwang <695331215@qq.com>
2021-02-22 20:14:12 +0800

    stl一元谓词

---
+++ c++-stl/一元谓词.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+template <typename T>
+class isDiv
+{
+public:
+    isDiv(T div){
+        this->divison = div; 
+    }
+    bool operator()(T &t)
+    {
+        return (t % divison == 0);
+    }
+
+private:
+    T divison;
+};
+
+void test()
+{
+    std::vector<int> v2;
+    for (int i = 10; i < 33; i++)
+    {
+        v2.push_back(i);
+    }
+    
+    int a = 4;
+    isDiv<int> mydiv(a); //函数对象
+    std::vector<int>::iterator it = find_if(v2.begin(), v2.end(), isDiv<int>(4)); //find_if返回的是一个迭代器
+    if (it != v2.end())
+    {
+        std::cout << "第一个被4整除的数是：" << *it << std::endl;
+    }
+}
+
+int main()
+{
+    test();
+    std::cout << "hello world" << std::endl;
+}
