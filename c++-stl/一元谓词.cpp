6b7532e8e292677fa0d90b1e19642888dec91b9e
zhjwang <695331215@qq.com>
2021-02-23 15:59:46 +0800

    一元谓词

--- c++-stl/一元谓词.cpp
+++ c++-stl/一元谓词.cpp
@@ -30,7 +30,9 @@ void test()
     
     int a = 4;
     isDiv<int> mydiv(a); //函数对象
+    // find_if(v2.begin(), v2.end(), mydiv;
     std::vector<int>::iterator it = find_if(v2.begin(), v2.end(), isDiv<int>(4)); //find_if返回的是一个迭代器
+
     if (it != v2.end())
     {
         std::cout << "第一个被4整除的数是：" << *it << std::endl;
