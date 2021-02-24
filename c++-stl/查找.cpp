fb4725479de95bed2e54a8c06d6c89e13ccd9a96
zhjwang <695331215@qq.com>
2021-02-24 20:44:27 +0800

    查找

--- c++-stl/查找.cpp
+++ c++-stl/查找.cpp
@@ -43,10 +43,47 @@ void test_binary_search()
         std::cout << "找到了！" << std::endl;
     }
 }
+void test_count()
+{
+    std::vector<int> vecInt;
+    vecInt.push_back(1);
+    vecInt.push_back(3);
+    vecInt.push_back(5);
+    vecInt.push_back(6);
+    vecInt.push_back(7);
+    vecInt.push_back(59);
+    int icount = std::count(vecInt.begin(), vecInt.end(), 7);
+    std::cout << icount << std::endl; //1
+}
+bool GreaterThree(int iNum)
+{
+    if (iNum >= 3)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void test_count_if()
+{
+    std::vector<int> vecInt;
+    vecInt.push_back(1);
+    vecInt.push_back(3);
+    vecInt.push_back(5);
+    vecInt.push_back(6);
+    vecInt.push_back(7);
+    vecInt.push_back(59);
+    int num = count_if(vecInt.begin(), vecInt.end(), GreaterThree);
+    std::cout << num << std::endl; //5
+}
 
 int main()
 {
     test_adjacent_find();
     test_binary_search();
+    test_count();
+    test_count_if();
     std::cout << "hello world" << std::endl;
 }
