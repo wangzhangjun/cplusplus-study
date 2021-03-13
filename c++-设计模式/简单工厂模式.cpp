f2314c4e2124c60388d99d5bc20b7f80e1d82c3d
zhjwang <695331215@qq.com>
2021-03-13 16:24:15 +0800

    简单工厂模式

---
+++ c++-设计模式/简单工厂模式.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class AbstractFruit {
+    public:
+        virtual void showName() = 0;
+};
+
+class Apple : public AbstractFruit{
+    public:
+        virtual void showName(){
+            cout << "apple." << endl;
+        }
+};
+
+class Banan : public AbstractFruit{
+public:
+    virtual void showName()
+    {
+        cout << "banan." << endl;
+    }
+};
+
+class FruitFactory{
+    public:
+        static AbstractFruit * createFruit(string flag) {
+            if(flag == "apple") {
+                return new Apple;
+            }else if(flag == "bana"){
+                return new Banan;
+            }
+        }
+};
+
+void test1()
+{
+    FruitFactory *factor = new FruitFactory;
+    AbstractFruit *fruit = factor->createFruit("apple");
+    fruit->showName();
+}
+
+int main()
+{
+    test1();
+    return 0;
+}
