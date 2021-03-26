ce27be1b3097140015a3e9033d7ff08e771b51ab
zhjwang <695331215@qq.com>
2021-03-26 16:58:16 +0800

    身份抽象类

---
+++ orderComputerRoom/Identity.h
@@ -0,0 +1,18 @@
+//
+// Created by zhjwang on 2021/3/26.
+//
+
+#ifndef ORDERCOMPUTERROOM_IDENTITY_H
+#define ORDERCOMPUTERROOM_IDENTITY_H
+#include "string"
+
+//身份抽象类
+class Identity {
+public:
+    std::string m_name;  //用户名
+    std::string m_pwd;   //密码
+    //操作菜单
+    virtual void operMenu() = 0;
+};
+
+#endif //ORDERCOMPUTERROOM_IDENTITY_H
