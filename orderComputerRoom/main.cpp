ad6e0da4328ec5dadaed8f3755dd07bbf4feb92b
zhjwang <695331215@qq.com>
2021-03-29 11:30:04 +0800

    login

--- orderComputerRoom/main.cpp
+++ orderComputerRoom/main.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include "Identity.h"
+#include "globalFile.h"
 using namespace  std;
+
+//登录功能
+void LoginIn(std::string fileName, int type)
+{
+    Identity *person = NULL;
+    ifstream  ifs;
+    ifs.open(fileName, ios::in);
+    if(!ifs.is_open()) {
+        cout << "文件不存在" << endl;
+        ifs.close();
+        return;
+    }
+    int id = 0;
+    std::string name;
+    std::string pwd;
+
+    if(type == 1) {
+        cout << "请输入学号：" <<endl;
+        cin >> id;
+    }else if(type == 2){
+        cout << "请输入教师职工号：" <<endl;
+        cin >> id;
+    }
+    cout << "请输入用户名：" << endl;
+    cin >> name;
+    cout << "请输入密码：" << endl;
+    cin >> pwd;
+
+    if (type == 1)
+    {
+        //学生登录验证
+    }
+    else if (type == 2)
+    {
+        //教师登录验证
+    }
+    else if(type == 3)
+    {
+        //管理员登录验证
+    }
+
+    cout << "验证登录失败!" << endl;
+    system("clear");
+    return;
+}
+
 int main() {
     int select  = 0;
     while(true) {
@@ -22,10 +71,13 @@ int main() {
         cin >> select;
         switch (select) {
             case 1:  //学生
+                LoginIn(STUDENT_FILE, 1);
                 break;
             case 2:  //老师
+                LoginIn(TEACHER_FILE, 1);
                 break;
             case 3:   //管理员
+                LoginIn(ADMIN_FILE, 1);
                 break;
             case 0:  //退出系统
                 cout << "欢迎下次使用！" << endl;
