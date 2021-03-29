7fce1fb1617253d5e63c74472cabd166971b692e
zhjwang <695331215@qq.com>
2021-03-29 11:11:34 +0800

    manager

---
+++ orderComputerRoom/manager.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zhjwang on 2021/3/29.
+//
+
+#include "manager.h"
+manager::manager() {}
+manager::manager(std::string name, std::string pwd) {}
+void manager::operMenu() {}
+void manager::addPerson() {}
+void manager::cleanFile() {}
---
+++ orderComputerRoom/manager.h
@@ -0,0 +1,30 @@
+//
+// Created by zhjwang on 2021/3/29.
+//
+
+#ifndef ORDERCOMPUTERROOM_MANAGER_H
+#define ORDERCOMPUTERROOM_MANAGER_H
+#include "Identity.h"
+
+class manager : public Identity{
+public:
+    manager();
+    manager(std::string name, std::string pwd);
+
+    //选择菜单
+    virtual void operMenu();
+    //添加账号
+    void addPerson();
+    //查看账号
+    void showPerson();
+
+    //查看机房信息
+    void showComputer();
+
+    //清空预约记录
+    void cleanFile();
+
+};
+
+
+#endif //ORDERCOMPUTERROOM_MANAGER_H
