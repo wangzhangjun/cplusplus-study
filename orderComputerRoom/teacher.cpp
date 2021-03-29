3295d22a6a24c7c2ca118da079d9e6389e8f989f
zhjwang <695331215@qq.com>
2021-03-29 11:04:36 +0800

    teacher

---
+++ orderComputerRoom/teacher.cpp
@@ -0,0 +1,15 @@
+//
+// Created by zhjwang on 2021/3/29.
+//
+
+#include "teacher.h"
+
+teacher::teacher() {}
+
+teacher::teacher(int empId, std::string name, std::string pwd) {}
+
+void teacher::showAllOrder() {}
+
+void teacher::operMenu() {}
+
+void teacher::validOrder() {}
---
+++ orderComputerRoom/teacher.h
@@ -0,0 +1,20 @@
+//
+// Created by zhjwang on 2021/3/29.
+//
+
+#ifndef ORDERCOMPUTERROOM_TEACHER_H
+#define ORDERCOMPUTERROOM_TEACHER_H
+#include "Identity.h"
+
+class teacher : public Identity {
+public:
+    teacher();
+    teacher(int empId, std::string name , std::string pwd);
+    virtual void operMenu (); //菜单界面
+    void showAllOrder();  //查看所有预约
+    void validOrder();  //审核预约
+    int m_emid;  //教师编号
+};
+
+
+#endif //ORDERCOMPUTERROOM_TEACHER_H
