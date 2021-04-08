2b056fb26563f7f50899cc29aa064f55b114c4d6
zhjwang <695331215@qq.com>
2021-04-08 14:58:57 +0800

    添加内存空间的计数器

---
+++ c++智能指针/实现智能指针.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class CStudent
+{
+public:
+    CStudent(const char *pszName);
+    CStudent(CStudent &obj);
+    ~CStudent();
+
+    CStudent &operator=(CStudent &obj);
+
+    void release();
+
+    void Show()
+    {
+        cout << hex << (int &)m_pszName << m_pszName << endl;
+    }
+
+private:
+    char * m_pszName;
+    int * m_pcount;  //添加一个变量，记录内存的使用次数
+};
+
+CStudent::CStudent(const char *pszName)
+{
+    m_pszName = new char[256];
+    m_pcount = new int;
+
+    strcpy(m_pszName, pszName);
+    *m_pcount = 1;
+}
+
+CStudent::CStudent(CStudent &obj)
+{
+    m_pszName = obj.m_pszName; //浅拷贝
+    m_pcount = obj.m_pcount;
+    (*m_pcount)++;
+    //strcpy(m_pszName, obj.m_pszName);
+}
+
+CStudent::~CStudent()
+{
+    release();
+}
+
+CStudent &CStudent::operator=(CStudent &obj)
+{
+    if(obj.m_pszName ==  m_pszName) {
+        return *this;
+    }
+    if (--(*m_pcount) == 0)
+    {
+        delete m_pszName;
+        m_pszName = NULL;
+        delete m_pcount;
+    }
+    m_pszName = obj.m_pszName; //浅拷贝
+    m_pcount = obj.m_pcount;
+    (*m_pcount)++;
+
+    return *this;
+}
+
+void CStudent::release()
+{
+    if (m_pszName != NULL && --*m_pcount == 0)
+    {
+        delete m_pszName;
+        m_pszName = NULL;
+        delete m_pcount;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    CStudent stu1("zhang san");
+    CStudent stu2("li si");
+    CStudent stu3 = stu2;
+
+    stu1.Show();
+    stu2.Show();
+    stu3.Show();
+    stu2.release();
+    stu3.Show();
+    return 0;
+}
