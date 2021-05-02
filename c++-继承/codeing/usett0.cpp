abd30d454da0dacbdb844d3f28e0e5f3ba58ac2e
zhjwang <695331215@qq.com>
2021-05-02 11:15:19 +0800

    继承的一些特殊用法

--- c++-继承/codeing/usett0.cpp
+++ c++-继承/codeing/usett0.cpp
@@ -3,9 +3,57 @@
 
 using namespace std;
 
+void show(TableTennisPlayer &rt)  //参数改为指针也是可以的
+{
+    rt.Name();
+    // rt.Rating();  这个是错误的，因为参数是基类，所以只能调用基类的方法
+}
 
+void test()
+{
+    //派生类和基础类之间的两种特殊关系
+    /*
+    第一种关系：派生类可以使用基类的方法，条件是方法不是私有的。
+    第二种关系：基类指针可以在不进行显示类型转换的情况下指向派生类对象，基类引用也可以指向派生类对象
+    注意：在第二种关系下，基类指针或者引用只能调用基类的方法。
+    注意2：不可以反着来，也就是把派生类的指针或者引用指向基类对象，因为一般来说派生类的内存空间大于基类
+    */
+
+    //这样其实会出现一些有趣的用法：
+    /*
+    第一种：基类引用或者指针做函数参数
+    */
+   TableTennisPlayer tp1("zhjwang", "laowang", false);
+   RatedPlayer rp1(1140, "Tom", "Tom1", false);
+   show(tp1);
+   show(rp1);
+   /*
+   第二种：用子类对象初始化父类对象
+   原理：
+        要初始化tp2，需要调用复制构造函数：TableTennisPlayer(const RatedPlayer &)
+        但是没有这样的构造函数，但是存在隐式的复制构造函数：
+        TableTennisPlayer（const TableTennisPlayer &）
+        符合父类引用或者指针指向子类对象。它会复制firstname, lastname, hasTable等成员。
+        其实也就是把tp2初始化我嵌套在rp2中的TableTennisPlayer对象
+   */
+   RatedPlayer rp2(1140, "Tom2", "Tom3", false);
+   TableTennisPlayer tp2(rp2);
+   tp2.Name();
+
+   /*
+   第三种：将子类对象赋值给父类对象,
+        这种情况下，程序将使用隐式的重载赋值运算符
+    TableTennisPlayer & operator=(const TableTennisPlayer &) const;
+   */
+   RatedPlayer rp3(1140, "Tom3", "Tom4", false);
+   TableTennisPlayer tp3("none", "none", false);
+   tp3 = rp3;
+   tp3.Name();
+}
 int main()
 {
+    test();
+    std::cout << "=====================" << std::endl;
     TableTennisPlayer p1("Chuck", "Blizzard", true);
     TableTennisPlayer p2("Tara", "Boomdea", false);
 
